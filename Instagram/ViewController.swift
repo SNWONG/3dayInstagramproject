6a61630f8fd018444cf3deb268074ebac9d659ce
FORyouMEtoKNOW <foryoumetoknow@foryoumetoknowdeimac.local>
2016-07-05 12:19:42 +0800

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by FORyouMEtoKNOW on 16/7/4.
+//  Copyright © 2016年 FORyouMEtoKNOW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        
+    }
+    
+
+
+}
+
